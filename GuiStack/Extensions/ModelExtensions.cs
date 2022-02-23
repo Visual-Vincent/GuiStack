92dad05ab9aed6f5b0d943bb2e1e9b40bb0c2ee6
Visual-Vincent <github-vv@mydoomsite.com>
2022-02-23 21:19:23 +0100

    Implemented S3 file download

---
+++ GuiStack/Extensions/ModelExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using GuiStack.Models;
+
+namespace GuiStack.Extensions
+{
+    public static class ModelExtensions
+    {
+        /// <summary>
+        /// Converts the <see cref="IEnumerable"/>&lt;<see cref="S3Object"/>&gt; to <see cref="IEnumerable"/>&lt;<see cref="S3ObjectModel"/>&gt;.
+        /// </summary>
+        /// <param name="objs">The collection to convert.</param>
+        /// <param name="bucketName">The name of the bucket that the objects belong to.</param>
+        public static IEnumerable<S3ObjectModel> ToObjectModel(this IEnumerable<S3Object> objs, string bucketName)
+        {
+            return objs.Select(obj => new S3ObjectModel() {
+                BucketName = bucketName,
+                Object = obj
+            });
+        }
+    }
+}
---
+++ GuiStack/Models/S3ObjectModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace GuiStack.Models
+{
+    public class S3ObjectModel
+    {
+        public string BucketName { get; set; }
+        public S3Object Object { get; set; }
+    }
+}

4f13dcdc3a911d00012de80344d9e16e969ba351
Visual-Vincent <github-vv@mydoomsite.com>
2022-03-09 22:33:10 +0100

    Implemented SQSQueueInfo

---
+++ GuiStack/Models/SQSQueueInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GuiStack.Models
+{
+    public class SQSQueueInfo
+    {
+        public int ApproximateNumberOfMessages { get; set; }
+        public int ApproximateNumberOfMessagesDelayed { get; set; }
+        public int ApproximateNumberOfMessagesNotVisible { get; set; }
+        public DateTime CreatedTimestamp { get; set; }
+        public int DelaySeconds { get; set; }
+        public DateTime LastModifiedTimestamp { get; set; }
+        public int MaximumMessageSize { get; set; }
+        public int MessageRetentionPeriod { get; set; }
+        public string QueueArn { get; set; }
+        public int ReceiveMessageWaitTimeSeconds { get; set; }
+        public int VisibilityTimeout { get; set; }
+        public bool FifoQueue { get; set; }
+    }
+}
--- GuiStack/Repositories/ISQSRepository.cs
+++ GuiStack/Repositories/ISQSRepository.cs
@@ -12,6 +12,8 @@ namespace GuiStack.Repositories
     public interface ISQSRepository
     {
         Task<IEnumerable<SQSQueue>> GetQueuesAsync();
+        Task<SQSQueueInfo> GetQueueAttributesAsync(string queueUrl);
+        Task<string> GetQueueUrlAsync(string queueName);
     }
 
     public class SQSRepository : ISQSRepository
@@ -26,6 +28,48 @@ namespace GuiStack.Repositories
             response.ThrowIfUnsuccessful("SQS");
 
             return response.QueueUrls.Select(url => new SQSQueue(url));
+        }
+
+        public async Task<SQSQueueInfo> GetQueueAttributesAsync(string queueUrl)
+        {
+            using var sqs = authenticator.Authenticate();
+            var response = await sqs.GetQueueAttributesAsync(queueUrl, new List<string>() {
+                "ApproximateNumberOfMessages", "ApproximateNumberOfMessagesDelayed", "ApproximateNumberOfMessagesNotVisible",
+                "CreatedTimestamp", "DelaySeconds", "LastModifiedTimestamp", "MaximumMessageSize", "MessageRetentionPeriod",
+                "QueueArn", "ReceiveMessageWaitTimeSeconds", "VisibilityTimeout", "FifoQueue"
+            });
+
+            response.ThrowIfUnsuccessful("SQS");
+
+            response.Attributes.TryGetValue("ReceiveMessageWaitTimeSeconds", out string ReceiveMessageWaitTimeSecondsStr);
+
+            if(!int.TryParse(ReceiveMessageWaitTimeSecondsStr, out var ReceiveMessageWaitTimeSeconds))
+                ReceiveMessageWaitTimeSeconds = -1;
+
+            return new SQSQueueInfo() {
+                ApproximateNumberOfMessages = response.ApproximateNumberOfMessages,
+                ApproximateNumberOfMessagesDelayed = response.ApproximateNumberOfMessagesDelayed,
+                ApproximateNumberOfMessagesNotVisible = response.ApproximateNumberOfMessagesNotVisible,
+                CreatedTimestamp = response.CreatedTimestamp,
+                DelaySeconds = response.DelaySeconds,
+                LastModifiedTimestamp = response.LastModifiedTimestamp,
+                MaximumMessageSize = response.MaximumMessageSize,
+                MessageRetentionPeriod = response.MessageRetentionPeriod,
+                QueueArn = response.QueueARN,
+                ReceiveMessageWaitTimeSeconds = ReceiveMessageWaitTimeSeconds,
+                VisibilityTimeout = response.VisibilityTimeout,
+                FifoQueue = response.FifoQueue,
+            };
+        }
+
+        public async Task<string> GetQueueUrlAsync(string queueName)
+        {
+            using var sqs = authenticator.Authenticate();
+            var response = await sqs.GetQueueUrlAsync(queueName);
+
+            response.ThrowIfUnsuccessful("SQS");
+
+            return response.QueueUrl;
         }
     }
 }

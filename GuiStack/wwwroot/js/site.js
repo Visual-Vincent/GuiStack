7495a4fc4db1171905eb793ec4033e3fecf1d949
Visual-Vincent <github-vv@mydoomsite.com>
2022-03-09 19:53:17 +0100

    Implemented navbar selected item regex

--- GuiStack/wwwroot/js/site.js
+++ GuiStack/wwwroot/js/site.js
@@ -1,4 +1,9 @@
-﻿function gs_DisplayError(message)
+﻿function isNull(obj)
+{
+    return obj === undefined || obj === null;
+}
+
+function gs_DisplayError(message)
 {
     document.getElementById("gs-error-banner").classList.add("visible");
     document.getElementById("gs-error-text").innerText = message;
@@ -8,3 +13,25 @@ function gs_CloseError()
 {
     document.getElementById("gs-error-banner").classList.remove("visible");
 }
+
+(function() {
+    var navs = document.querySelectorAll(".gs-navbar > a");
+    var currentUrl = window.location.pathname.split("?")[0];
+
+    for(var i = 0; i < navs.length; i++)
+    {
+        var nav = navs[i];
+        var selectionRegex = nav.getAttribute("data-selected-regex");
+
+        if(isNull(selectionRegex) || selectionRegex.length <= 0)
+            continue;
+
+        var regex = new RegExp(selectionRegex);
+
+        if(regex.test(currentUrl))
+        {
+            nav.classList.add("selected");
+            break;
+        }
+    }
+})();

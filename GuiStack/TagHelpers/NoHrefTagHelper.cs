d103be6fb92eff54d06a935efb1b6091f7e535c2
Visual-Vincent <github-vv@mydoomsite.com>
2022-03-10 19:43:55 +0100

    Tag helper for non-href links

---
+++ GuiStack/TagHelpers/NoHrefTagHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace GuiStack.TagHelpers
+{
+    [HtmlTargetElement("a", Attributes = "no-href")]
+    public class NoHrefTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.RemoveAll("no-href");
+
+            if(context.AllAttributes.ContainsName("href"))
+                return;
+
+            output.Attributes.Add("href", "javascript:void(0)");
+        }
+    }
+}

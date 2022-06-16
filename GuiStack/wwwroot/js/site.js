1129f9b92a5e0e8a35f79661b958202575a78a66
Visual-Vincent <github-vv@mydoomsite.com>
2022-06-16 21:07:14 +0200

    Implemented frontend support for sending Protobuf messages

--- GuiStack/wwwroot/js/site.js
+++ GuiStack/wwwroot/js/site.js
@@ -150,11 +150,47 @@ function gs_FindParent(element, selector, throwNotFound)
     return parent;
 }
 
+function gs_GetProtobufMessage(path)
+{
+    if(isNull(path))
+        throw "'path' cannot be null";
+
+    if(typeof path !== "string")
+        throw "'path' must be a string";
+
+    if(!path.startsWith("proto/"))
+        throw "'" + path + "' is not a valid protobuf definition path";
+
+    return localStorage.getItem(path);
+}
+
+function gs_GetProtobufRootDefinition(path)
+{
+    if(isNull(path))
+        throw "'path' cannot be null";
+
+    if(typeof path !== "string")
+        throw "'path' must be a string";
+
+    if(!path.startsWith("proto/"))
+        throw "'" + path + "' is not a valid protobuf definition path";
+
+    root = path.split("/")[1];
+
+    if(root.length <= 0)
+        return null;
+
+    return localStorage.getItem("protoroot/" + root);
+}
+
 function gs_RefreshProtobufList(element)
 {
+    if(isNull(element))
+        throw "'element' cannot be null";
+
     if(!isElement(element) || !(element instanceof HTMLUListElement))
         throw "'element' must be an <ul> element";
-    
+
     if(
         !element.classList.contains("gs-tree") || 
         !element.classList.contains("gs-protobuf-list") ||
@@ -162,8 +198,35 @@ function gs_RefreshProtobufList(element)
         element.getAttribute("data-proto-list-id").length <= 0
     )
         throw "The specified element is not a valid protobuf definitions list (id=" + element.id + ")";
-    
+
     __gs_DynamicFunctions["gs_RefreshProtobufList_" + element.getAttribute("data-proto-list-id")]();
+}
+
+function gs_SelectOption(selectElement, option)
+{
+    if(isNull(selectElement))
+        throw "'selectElement' cannot be null";
+
+    if(isElement(selectElement))
+    {
+        if(!(selectElement instanceof HTMLSelectElement))
+            throw "'selectElement' must be a <select> element";
+
+        selectElement.value = option;
+        selectElement.dispatchEvent(new Event("change"));
+    }
+    else if(selectElement instanceof jQuery || selectElement instanceof $ || selectElement.jquery)
+    {
+        if(!selectElement.is("select"))
+            throw "'selectElement' must be a <select> element";
+
+        selectElement.val(option);
+        selectElement.trigger("click");
+    }
+    else
+    {
+        throw "'selectElement' must be a <select> element";
+    }
 }
 
 function gsevent_InfoTable_ToggleButton_Click(event)
@@ -299,3 +362,91 @@ $(document).ready(function() {
         __gs_bodyDragCounter = 0;
     });
 });
+
+/*
+    The code for the "gs_Uint8ArrayToBase64" function below was originally written
+    by Jon Leighton, and can be found at: https://gist.github.com/jonleighton/958841
+
+    This solves an issue I've been facing for many years in JavaScript.
+    Thank you, Jon, for creating this function!
+    - Visual Vincent, 2022-06-16
+
+
+    The following snippet of code is licensed under the MIT license:
+
+        Copyright (c) 2011 Jon Leighton
+
+        Permission is hereby granted, free of charge, to any person obtaining a copy
+        of this software and associated documentation files (the "Software"), to deal
+        in the Software without restriction, including without limitation the rights
+        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        copies of the Software, and to permit persons to whom the Software is
+        furnished to do so, subject to the following conditions:
+
+        The above copyright notice and this permission notice shall be included in all
+        copies or substantial portions of the Software.
+
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        SOFTWARE.
+*/
+
+function gs_Uint8ArrayToBase64(bytes)
+{
+    var base64    = ''
+    var encodings = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+
+    var byteLength    = bytes.byteLength
+    var byteRemainder = byteLength % 3
+    var mainLength    = byteLength - byteRemainder
+
+    var a, b, c, d
+    var chunk
+
+    // Main loop deals with bytes in chunks of 3
+    for (var i = 0; i < mainLength; i = i + 3)
+    {
+        // Combine the three bytes into a single integer
+        chunk = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2]
+
+        // Use bitmasks to extract 6-bit segments from the triplet
+        a = (chunk & 16515072) >> 18 // 16515072 = (2^6 - 1) << 18
+        b = (chunk & 258048)   >> 12 // 258048   = (2^6 - 1) << 12
+        c = (chunk & 4032)     >>  6 // 4032     = (2^6 - 1) << 6
+        d = chunk & 63               // 63       = 2^6 - 1
+
+        // Convert the raw binary segments to the appropriate ASCII encoding
+        base64 += encodings[a] + encodings[b] + encodings[c] + encodings[d]
+    }
+  
+    // Deal with the remaining bytes and padding
+    if (byteRemainder == 1)
+    {
+        chunk = bytes[mainLength]
+
+        a = (chunk & 252) >> 2 // 252 = (2^6 - 1) << 2
+
+        // Set the 4 least significant bits to zero
+        b = (chunk & 3)   << 4 // 3   = 2^2 - 1
+
+        base64 += encodings[a] + encodings[b] + '=='
+    }
+    else if (byteRemainder == 2)
+    {
+        chunk = (bytes[mainLength] << 8) | bytes[mainLength + 1]
+
+        a = (chunk & 64512) >> 10 // 64512 = (2^6 - 1) << 10
+        b = (chunk & 1008)  >>  4 // 1008  = (2^6 - 1) << 4
+
+        // Set the 2 least significant bits to zero
+        c = (chunk & 15)    <<  2 // 15    = 2^4 - 1
+
+        base64 += encodings[a] + encodings[b] + encodings[c] + '='
+    }
+
+    return base64
+}

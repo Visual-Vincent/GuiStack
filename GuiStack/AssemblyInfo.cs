9fa1f17b2d13cf92a68b9ccd436a1ce0de930e61
Visual-Vincent <github-vv@mydoomsite.com>
2022-09-30 00:22:27 +0200

    Upgraded to .NET 6.0, added AssemblyInfo + a missing license file

---
+++ GuiStack/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+
+[assembly: AssemblyTitle("GuiStack - A GUI for your AWS or LocalStack environment")]
+[assembly: AssemblyProduct("GuiStack")]
+[assembly: AssemblyCopyright("Copyright © Vincent Bengtsson & Contributors 2022")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]

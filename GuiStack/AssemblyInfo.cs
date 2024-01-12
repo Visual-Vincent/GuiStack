406900e6805e951d946875387dafc693e427a80a
Visual-Vincent <github-vv@mydoomsite.com>
2024-01-12 14:47:57 +0100

    Updated copyright

--- GuiStack/AssemblyInfo.cs
+++ GuiStack/AssemblyInfo.cs
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  * 
- * Copyright © Vincent Bengtsson & Contributors 2022
+ * Copyright © Vincent Bengtsson & Contributors 2022-2024
  * https://github.com/Visual-Vincent/GuiStack
  */
 
@@ -13,6 +13,6 @@ using System.Resources;
 
 [assembly: AssemblyTitle("GuiStack - A GUI for your AWS or LocalStack environment")]
 [assembly: AssemblyProduct("GuiStack")]
-[assembly: AssemblyCopyright("Copyright © Vincent Bengtsson & Contributors 2022")]
+[assembly: AssemblyCopyright("Copyright © Vincent Bengtsson & Contributors 2022-2024")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: NeutralResourcesLanguage("en")]

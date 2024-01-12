93d62b435d19aaafc237395c2bd57c499ee2aa24
Visual-Vincent <github-vv@mydoomsite.com>
2024-01-12 17:06:54 +0100

    Added SNS endpoint environment variables to default scripts

--- scripts/default-run-docker.sh
+++ scripts/default-run-docker.sh
@@ -4,6 +4,7 @@ export AWS_ACCESS_KEY_ID=localstack
 export AWS_SECRET_ACCESS_KEY=localstack
 export AWS_REGION=eu-central-1
 export AWS_S3_ENDPOINT_URL=http://host.docker.internal:4566
+export AWS_SNS_ENDPOINT_URL=http://host.docker.internal:4566
 export AWS_SQS_ENDPOINT_URL=http://host.docker.internal:4566
 export AWS_S3_FORCE_PATH_STYLE=true
 
@@ -12,6 +13,7 @@ docker run -p 5000:80 \
     -e AWS_SECRET_ACCESS_KEY \
     -e AWS_REGION \
     -e AWS_S3_ENDPOINT_URL \
+    -e AWS_SNS_ENDPOINT_URL \
     -e AWS_SQS_ENDPOINT_URL \
     -e AWS_S3_FORCE_PATH_STYLE \
   --add-host=host.docker.internal:host-gateway \

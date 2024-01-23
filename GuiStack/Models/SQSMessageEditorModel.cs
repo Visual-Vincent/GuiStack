607417bbdfc3775b8217801ae2b28647186210e0
Visual-Vincent <github-vv@mydoomsite.com>
2024-01-23 16:54:55 +0100

    Extracted the SQS message editor to a separate partial page

---
+++ GuiStack/Models/SQSMessageEditorModel.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ * 
+ * Copyright © Vincent Bengtsson & Contributors 2022-2024
+ * https://github.com/Visual-Vincent/GuiStack
+ */
+
+using System;
+
+namespace GuiStack.Models
+{
+    public class SQSMessageEditorModel
+    {
+        public string ElementId { get; set; }
+        public string EditorVariable { get; set; }
+        public string ContainerVariable { get; set; }
+
+        public SQSMessageEditorModel(string elementId, string containerVariable, string editorVariable)
+        {
+            ElementId = elementId;
+            ContainerVariable = containerVariable;
+            EditorVariable = editorVariable;
+        }
+    }
+}
